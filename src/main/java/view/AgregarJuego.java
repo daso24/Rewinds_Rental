bd69aa6c5f47832c3fcaf7ea4b517d4abe8521d8
daso24 <daso_24@alu.uabcs.mx>
2026-06-05 11:19:08 -0700

    correccion de errores

--- src/main/java/view/AgregarJuego.java
+++ src/main/java/view/AgregarJuego.java
@@ -170,6 +170,7 @@ public class AgregarJuego extends JFrame
         crearLabel(contentPanel, "Año:", 640, 360);
         cbAnio = crearCombo(contentPanel, new String[]{"2026", "2025", "2024", "2023", "2022", "2021", "2020", "2019", "2018", "2017", "2016", "2015", "2014", "2013", "2012", "2011", "2010", "2005", "2000", "1995", "1990"}, 640, 385, 110);
         contenedorCentral.add(contentPanel, BorderLayout.CENTER);
+        cbAnio.setEditable(true);
 
         btnAgregar = new JButton("Agregar Videojuego")
         {
